6208dd505adcf3932bf3a84268e9a109c1b84632
Denys Piuro <denys.pyuro@gmail.com>
2024-10-12 15:02:55 +0300

    Configured throttling for API requests

--- theatre_api_service/settings.py
+++ theatre_api_service/settings.py
@@ -136,4 +136,8 @@ REST_FRAMEWORK = {
     ),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
         "PAGE_SIZE": 5,
+    "DEFAULT_THROTTLE_RATES": {
+            "anon": "2/day",
+            "user": "3/day",
+    },
 }
