677a509816bd20114367675d977fad5fee2aeb95
Denys Piuro <denys.pyuro@gmail.com>
2024-10-13 15:37:10 +0300

    Add models to admin.py

--- theatre/admin.py
+++ theatre/admin.py
@@ -1,7 +1,40 @@
 from django.contrib import admin
-from theatre.models import Performance
+
+from theatre.models import (
+    Actor,
+    Genre,
+    Play,
+    TheatreHall,
+    Performance,
+    Reservation,
+    Ticket
+)
+
+
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name")
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+@admin.register(Play)
+class PlayAdmin(admin.ModelAdmin):
+    list_display = ("title", "description")
+
+@admin.register(TheatreHall)
+class TheatreHallAdmin(admin.ModelAdmin):
+    list_display = ("name", "rows", "seats_in_row")
 
 @admin.register(Performance)
 class PerformanceAdmin(admin.ModelAdmin):
     list_display = ("play", "theatre_hall", "show_time")
 
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ("user", "created_at")
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ("row", "seat", "performance", "reservation")
